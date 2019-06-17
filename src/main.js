37fbc00136b37b3fcc3924aeea70c9285c45b988
tuanmjnh <minhtuan200990@gmail.com>
2019-06-17 20:23:20 +0700

    tm

--- src/main.js
+++ src/main.js
@@ -1,30 +1,35 @@
-import '@babel/polyfill'
-import Vue from 'vue'
-import './plugins/vuetify'
-import App from './App.vue'
+// import '@babel/polyfill';
+import Vue from 'vue';
+// vuetify
+// import './plugins/vuetify'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import App from './App.vue';
 // jQuery
 import $ from 'jquery';
 window.jQuery = $;
 window.$ = $;
 // Helpers
-import helpers from './plugins/helpers'
+import helpers from './plugins/helpers';
 // Mixins
-import mixins from './plugins/mixins'
+import mixins from './plugins/mixins';
 // Store
-import store from './store'
+import store from './store';
 // Router
-import router from './router'
+import router from './router';
 // Filters
-import filters from './plugins/filters'
+import filters from './plugins/filters';
 // Directive
-import directive from './plugins/directive'
+import directive from './plugins/directive';
 // Firebase
-import { FBAuth } from './plugins/firebaseInit.js'
-import VueFirestore from 'vue-firestore'
-Vue.use(VueFirestore)
+import { FBAuth } from './plugins/firebaseInit.js';
+import VueFirestore from 'vue-firestore';
+// Use
+Vue.use(VueFirestore);
+Vue.use(ElementUI);
 // Components
-Vue.config.productionTip = false
-let app
+Vue.config.productionTip = false;
+let app;
 FBAuth.onAuthStateChanged(user => {
   // store.commit("auth/authUpdate", user)
   // console.log(user)
@@ -34,6 +39,6 @@ FBAuth.onAuthStateChanged(user => {
       store,
       router,
       render: h => h(App)
-    }).$mount('#app')
+    }).$mount('#app');
   }
-})
+});
